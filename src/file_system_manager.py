7fd71a9ee37e03f194c139f9c5a2cde437f6eae2
Cristian Dinu <cristian.dinu@hypersay.com>
2023-03-29 09:35:45 +0100

    [refactor] Remove unused function

--- src/file_system_manager.py
+++ src/file_system_manager.py
@@ -15,28 +15,3 @@ def create_directory_structure(output_folder, slide_data):
     os.makedirs(output_path, exist_ok=True)
 
     return output_path
-
-
-def extract_images(slide_data, output_folder):
-    """
-    Extract images from the slide and save them in the output folder.
-    Return a list of image paths.
-    """
-    image_paths = []
-    pptx = Presentation(slide_data["pptx_path"])
-    slide = pptx.slides[slide_data["slide_index"]]
-
-    for idx, shape in enumerate(slide.shapes):
-        if shape.shape_type == 13:  # Picture shape type
-            image = shape.image
-            img_path = os.path.join(
-                output_folder, f"{slide_data['title']}-{idx + 1}.png")
-
-            with open(img_path, "wb") as img_file:
-                img_file.write(image.blob)
-
-            # Store the relative path of the image
-            relative_img_path = f"./{slide_data['title']}-{idx + 1}.png"
-            image_paths.append(relative_img_path)
-
-    return image_paths
